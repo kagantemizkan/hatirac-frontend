df3bfc7801911f1915142c3981b2bb02aa0c1a56
yigit-taskin-jotform <yigittaskin@jotform.com>
2023-09-21 22:13:20 +0300

    edit: marquee

--- src/components/HomeComponents/Marquee.js
+++ src/components/HomeComponents/Marquee.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import '../../styles/HomeStyles/marquee.scss'; // Yukarıdaki CSS kodunu içeren dosyanın yolunu düzeltin
+import React, { useEffect, useRef } from 'react';
+import '../../styles/HomeStyles/marquee.scss'
 
 function Marquee() {
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+    const marquee = marqueeRef.current;
+    const container = marquee.parentElement;
+
+    const animateMarquee = () => {
+      const containerWidth = container.offsetWidth;
+      const marqueeWidth = marquee.offsetWidth;
+
+      const animation = marquee.animate(
+        [
+          { transform: `translateX(${containerWidth}px)` },
+          { transform: `translateX(-${marqueeWidth}px)` },
+        ],
+        {
+          duration: 25000, // Kayan hızı ayarlayın (milisaniye cinsinden)
+          iterations: Infinity, // Sonsuz tekrar
+          easing: 'linear', // Lineer hareket
+        }
+      );
+
+      animation.play();
+    };
+
+    animateMarquee();
+  }, []);
+
   return (
     <div className="marquee-container">
-      <span className="marquee-text">GELECEKTEKİ 'SEN'e HATIRA BIRAK - SEVDİĞİN KİŞİYE GELECEKTE GÖRMESİ İÇİN HATIRA YOLLA - GELECEKTEKİ 'SEN'e HATIRA BIRAK - SEVDİĞİN KİŞİYE GELECEKTE GÖRMESİ İÇİN HATIRA YOLLA - GELECEKTEKİ 'SEN'e HATIRA BIRAK - SEVDİĞİN KİŞİYE GELECEKTE GÖRMESİ İÇİN HATIRA YOLLA</span>
+      <div
+        className="marquee"
+        ref={marqueeRef}
+        style={{ whiteSpace: 'nowrap', display: 'inline-block' }}>
+        <span>TÜM ÜRÜNLERDE %20 İNDİRİM <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+        <span>SEVDİĞİNİZE VERECEĞİNİZ, ZAMANIN ÖTESİNDE BİR ARMAĞAN <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+        <span>HATIRALARINIZI HEDİYE OLARAK VERMEKTEN DAHA GÜZEL NE OLABİLİR? <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+        <span>TÜM ÜRÜNLERDE %20 İNDİRİM <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+        <span>HEM FİZİKSEL HEM DİJİTAL ÜRÜNLER <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+        <span>HEMEN ŞİMDİ SATIN AL, İNDİRİMDEN FAYDALAN! <i class="fa-regular fa-heart" style={{ color: '#F9D973' }}></i> </span>
+      </div>
     </div>
   );
 }
